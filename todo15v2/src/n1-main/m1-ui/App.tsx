b1737c2b141b8520f43e54a58ff91a9660f04ecb
DinaSem <neispravomayas1@gmail.com>
2022-03-03 21:13:16 +0300

    podgotovka

---
+++ todo15v2/src/n1-main/m1-ui/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './App.css'
+import { TodolistsList } from '../../n2-features/TodolistsList/TodolistsList'
+
+// You can learn about the difference by reading this guide on minimizing bundle size.
+// https://mui.com/guides/minimizing-bundle-size/
+// import { AppBar, Button, Container, IconButton, Toolbar, Typography } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import { Menu } from '@mui/icons-material';
+
+
+function App() {
+
+    return (
+        <div className="App">
+
+        </div>
+    )
+}
+
+export default App
